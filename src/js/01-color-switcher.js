a1548a57ee35be1f41ff7ece61694c6af6e2d8bb
Vitaliy <103895059+vitaliy197811@users.noreply.github.com>
2022-09-22 20:03:49 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+let timerId = null;
 
+startBtnRef.addEventListener('click', () => {
+    timerId = setInterval(startRandomColor, 1000)
+    startBtnRef.disabled = true;
+    stopBtnRef.disabled = false;
+});
+
+function startRandomColor(e) {
+    document.body.style.backgroundColor = getRandomHexColor();
+    function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+};
+
+stopBtnRef.addEventListener('click', () => {
+    clearInterval(timerId)
+    startBtnRef.disabled = false;
+    stopBtnRef.disabled = true;
+})
