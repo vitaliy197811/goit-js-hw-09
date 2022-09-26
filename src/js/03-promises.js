878e5d0d2958e95f8687fe330c9b54c1090e892d
Vitaliy <103895059+vitaliy197811@users.noreply.github.com>
2022-09-26 19:23:37 +0300

    alert

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,11 +10,16 @@ function onFormSubmit(e) {
   let inputStep = Number(document.querySelector('[name="step"]').value);
   let inputAmount = Number(document.querySelector('[name="amount"]').value);
   
-  for (let position = 1; position <= inputAmount; position += 1) {
-    createPromise(position, inputDelay)
+  if ( inputDelay >= 0 && inputStep >= 0 && inputAmount >= 0 ) {
+    for (let position = 1; position <= inputAmount; position += 1) {
+      createPromise(position, inputDelay)
       .then((position, delay) => Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
       .catch((position, delay) => Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
-    inputDelay += inputStep;
+      inputDelay += inputStep;
+    }
+  } else {
+    window.alert("Всі значення у полях не повинні бути від'ємними");
+    return;
   }
 }
 
@@ -30,3 +35,4 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
+
